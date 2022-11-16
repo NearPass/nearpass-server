5b144499b2471f1926ded8ce080fabf8c7e781d4
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-16 20:51:47 +0530

    feat: add keypair generation

--- utils.js
+++ utils.js
@@ -5,6 +5,19 @@ const generateKeyPair = () => {
     return keyPair;
 };
 
+const addKeyPair = async (ctx, networkId, accountId, keyPair) => {
+    const content = {
+        account_id: accountId,
+        public_key: keyPair.publicKey.toString(),
+        private_key: keyPair.toString(),
+    };
+    await writeFile(
+        `${__dirname}/.near-credentials/${networkId}/${ctx.from.username}_${accountId}.json`,
+        JSON.stringify(content)
+    );
+    return content.private_key;
+};
+
 module.exports = {
     generateKeyPair,
 };
