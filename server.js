2704dd9a5c14b3f1857d5cbe5c8700e7c9bbc531
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-16 20:52:07 +0530

    feat: add connect wallet and ticket redeem endpoints

--- server.js
+++ server.js
@@ -2,19 +2,97 @@ const express = require("express");
 const { connect } = require("near-api-js");
 const getConfig = require("./config");
 const { generateKeyPair } = require("./utils");
+var cors = require("cors");
+const { UnencryptedFileSystemKeyStore } = require("near-api-js/lib/key_stores");
+
+let keyStore;
+let near;
+
+const EVENTS_CONTRACT_ADDRESS = "dev-1668157708044-69561609686629";
 
 async function init() {
     const nearConfig = getConfig("testnet");
-    let near = await connect({ ...nearConfig });
+    keyStore = new UnencryptedFileSystemKeyStore("./keyStore");
+    near = await connect({ keyStore, ...nearConfig });
 }
 
-const app = express();
+const isKeyAdded = async (near, accountId, publicKey) => {
+    const account = await near.account(accountId);
+    const keys = await account.getAccessKeys();
+    let result = keys.filter((key) => {
+        return key.public_key == publicKey;
+    });
+    return result ? 1 : 0;
+};
 
-app.get("/", (req, res, next) => {
+const eventsContractInstance = (account) => {
+    const eventsContract = new nearAPI.Contract(
+        account,
+        EVENTS_CONTRACT_ADDRESS,
+        {
+            changeMethods: ["redeem"],
+            sender: account,
+        }
+    );
+    return eventsContract;
+};
+
+const redeemCall = async (eventsContract, ticketId) => {
+    const tx = await eventsContract.redeem({
+        args: {
+            ticketId,
+        },
+    });
+
+    return tx;
+};
+
+const app = express();
+app.use(express.json());
+
+app.get("/connectwallet", async (req, res, next) => {
+    let { accountId } = req.query;
+
+    let key = await near.config.keyStore.getKey("testnet", accountId);
+
+    if (key) {
+        let public_key = key.getPublicKey();
+        if (isKeyAdded(near, accountId, public_key)) {
+            return res.status(200).json({
+                connected: true,
+            });
+        }
+    }
+
     let keyPair = generateKeyPair();
     let public_key = keyPair.getPublicKey().toString();
+    await near.config.keyStore.setKey("testnet", accountId, keyPair);
+
+    let url = new URL("https://wallet.testnet.near.org/login");
+    url.searchParams.set("public_key", public_key);
+    url.searchParams.set("contract_id", EVENTS_CONTRACT_ADDRESS);
+    url.searchParams.append("methodNames", "createEvent");
+    url.searchParams.append("methodNames", "redeem");
+
     res.status(200).json({
-        sign_in_link: `https://wallet.testnet.near.org/login?title=tgbot&public_key=${public_key}&methodNames=`,
+        sign_in_link: url,
+    });
+});
+
+app.get("/redeem", async (req, res, next) => {
+    let { accountId, ticketId } = req.query;
+    let account = await near.account(accountId);
+    let eventsContract = eventsContractInstance(account);
+    let tx = await redeemCall(eventsContract, ticketId);
+    console.log(tx);
+    return res.status(200).json({
+        data: tx,
+    });
+});
+
+app.get("/", (req, res, next) => {
+    res.status(200).json({
+        sign_in_link: `https://wallet.testnet.near.org/login?title=tgbot&public_key=&methodNames=`,
     });
 });
 
